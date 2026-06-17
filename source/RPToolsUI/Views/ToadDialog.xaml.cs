75bd9a31743fccaeda6bb67d35151637b68693bb
Александр Борисов <alexandrborisov394@gmail.com>
2026-06-17 15:06:24 +0300

    add sizetocontext property to RPToolsUI TaskDialog

--- source/RPToolsUI/Views/ToadDialog.xaml.cs
+++ source/RPToolsUI/Views/ToadDialog.xaml.cs
@@ -1,7 +1,7 @@
+using System;
 using System.Windows;
 using System.Windows.Input;
 using Wpf.Ui.Controls;
-using RPToolsUI.Models;
 using RPToolsUI.Services;
 using Wpf.Ui.Appearance;
 
@@ -9,10 +9,16 @@ namespace RPToolsUI.Views;
 
 public partial class ToadDialog : FluentWindow
 {
+        private const double MaxDialogWidthRatio = 0.7;
+        private const double MaxDialogHeightRatio = 0.8;
+        private const double ContentWidthPadding = 100;
+        private const double ContentHeightPadding = 160;
+
         public ToadDialog ()
         {
             InitializeComponent();
             
+            ApplySizeLimits();
             
             Loaded += (_, __) =>
             {
@@ -29,6 +35,19 @@ public partial class ToadDialog : FluentWindow
             // ThemeWatcherService.Initialize();
             // ThemeWatcherService.ApplyTheme(ApplicationTheme.Dark);
             ThemeWatcherService.Watch(this);
+        }
+
+        private void ApplySizeLimits()
+        {
+            var workArea = SystemParameters.WorkArea;
+
+            MaxWidth = Math.Max(MinWidth, workArea.Width * MaxDialogWidthRatio);
+            MaxHeight = Math.Max(MinHeight, workArea.Height * MaxDialogHeightRatio);
+
+            var maxContentWidth = Math.Max(250, MaxWidth - ContentWidthPadding);
+            TitleTextBlock.MaxWidth = maxContentWidth;
+            MessageTextBlock.MaxWidth = maxContentWidth;
+            MessageScrollViewer.MaxHeight = Math.Max(80, MaxHeight - ContentHeightPadding);
         }
         
         private void RootGrid_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
